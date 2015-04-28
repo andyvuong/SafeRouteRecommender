800e371e859ffca201c4f3b6d09d0ca8b79731a8
kouldisha <kouldisha@gmail.com>
2015-04-28 16:34:42 -0500

    added comments
    got rid of OSM stuff (not relevant anymore since we are going to use
    google maps API)

--- routes.py
+++ routes.py
@@ -3,6 +3,8 @@ import googlemaps
 from googlemaps import Client as client
 import json  
 
+#be sure to install the Python client for the Google maps API, here is the link: https://developers.google.com/api-client-library/python/start/installation
+
 client = googlemaps.Client(key="AIzaSyDAszXFz4LjQQ6Itr4o7O90RUJhUhhnXXI")
 #service = build('api_name', 'api_version', ...)
 
